a44f435c609523b8e058b5a7e75602a111a882ad
CarlosSG4 <raptor_fx_42@hotmail.com>
2019-03-09 16:09:06 -0600

    error_1

---
+++ controlde_citas/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for controldecitas project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controldecitas.settings')
+
+application = get_wsgi_application()
