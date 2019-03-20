aa4870e771ae46e922ec1f4d94a3f5a5665b1c3b
CarlosSG4 <raptor_fx_42@hotmail.com>
2019-03-19 20:28:40 -0600

    creacion de usuario y modificacion en la base de datos

--- core/models.py
+++ core/models.py
@@ -1,28 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-
-class perfil(models.Model):
-    name = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    apellidos = models.CharField(max_length=200)
-    edad = models.CharField(max_length=200)
-    curp = models.CharField(max_length=200)
-    sexo = models.CharField(max_length=200)
-    celular = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-    direccion = models.CharField(max_length=200)
-    municipio = models.CharField(max_length=200)
-    estado = models.CharField(max_length=200)
-    consultorio = models.CharField(max_length=200)
-    medico = models.CharField(max_length=200)
-    turno = models.CharField(max_length=200)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+class especialidades(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- core/views.py
+++ core/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render, HttpResponse
-
-html_base = """
-<h1>Control de Citas</h1>
-<ul>
-    <li><a href="/">Inicio</a></li>
-    <li><a href="/about/">Acerca de</a></li>
-
-"""
+from django.shortcuts import render
+from .models import especialidades
 # Create your views here.
 
 def home(request):
-    return render(request, "core/home.html")
+    context = {
+        'posts': especialidades.objects.all()
+    }
+    return render(request, "core/home.html", context)
 
